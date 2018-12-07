24c9cb753f84df87df5dea2b9e3f4e9f57df2022
lucacimo <luca.cimonetti@gmail.com>
2018-12-07 13:32:10 +0300

    added socket.io solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import request, render_template, redirect
+from flask import request, render_template, session
 from flask_wtf import FlaskForm
+from flask_socketio import SocketIO
 from wtforms import StringField, SubmitField, IntegerField, ValidationError
 from wtforms.validators import DataRequired, NumberRange
 import phonenumbers
@@ -12,6 +13,9 @@ import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+socketio = SocketIO(app)
+
+transactions = {}
 
 
 def validate_phone(form, field):
@@ -32,37 +36,75 @@ class MpesaForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+@socketio.on('connection')
+def handle_my_custom_event(message):
+    print(message)
+
+
+@socketio.on('submission')
+def handle_my_custom_event(message):
+    message = json.loads(message)
+    phone_number = message['phone_number'].replace("+", "")
+    amount = message['amount']
+    timestamp = str(time.strftime("%Y%m%d%H%M%S"))
+    passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+
+    password = base64.b64encode(bytes(u'174379' + passkey + timestamp, 'UTF-8')).decode('UTF-8')
+
+    consumer_key = "FlYVIVkqtno6joTyU045VDDChM39eWFq"
+    consumer_secret = "9NuFmdRGLS1ZDAEk"
+    api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+    y = json.loads(requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret)).text)
+
+    access_token = "{}".format(y['access_token'])
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer {}".format(access_token)}
+    body = {
+        "BusinessShortCode": "174379",
+        "Password": "{}".format(password),
+        "Timestamp": "{}".format(timestamp),
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "{}".format(amount),
+        "PartyA": "{}".format(phone_number),
+        "PartyB": "174379",
+        "PhoneNumber": "{}".format(phone_number),
+        "CallBackURL": "https://mpesatestapp.herokuapp.com/callback",
+        "AccountReference": "account",
+        "TransactionDesc": "account"
+    }
+
+    response = requests.post(api_url, json=body, headers=headers)
+    response = json.loads(response.text)
+    if response['ResponseCode'] == '0':
+        transactions[request.sid] = response['CheckoutRequestID']
+
+    print(transactions)
+
+
 @app.route('/callback', methods=['POST'])
 def api_message():
     data = request.data
     data = json.loads(data)
+    print(data)
 
-    if (data['Body']['stkCallback']['ResultCode']) == 0:
-        print("Payment Successfull")
-        return "Successful"
-    else:
-        print("Payment Failed, Try again")
-        return "Failed"
+    # if (data['Body']['stkCallback']['ResultCode']) == 0:
+    #     print("Payment Successful")
+    #     return "Successful"
+    # else:
+    #     print("Payment Failed, Try again")
+    #     return "Failed"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def submit():
     form = MpesaForm()
-    if form.validate_on_submit():
-        api_url = "https://mpesatestapp.herokuapp.com/mpesa"
-        phone_number = form.phone.data.replace("+", "")
-        body = {
-                "phone_number": "{}".format(phone_number),
-                "amount": "{}".format(form.amount.data),
-        }
-        response = requests.post(api_url, json=body)
-
     return render_template('mpesaform.html', title='Mpesa Payment', form=form)
 
 
 @app.route('/mpesa', methods = ['POST'])
 def send_push_request():
-    phone_number = request.json.get('phone_number')
+    phone_number = request.json.get('phone_number').replace("+", "")
     amount = request.json.get('amount')
 
     timestamp = str(time.strftime("%Y%m%d%H%M%S"))
@@ -95,8 +137,6 @@ def send_push_request():
 
     response = requests.post(api_url, json=body, headers=headers)
 
-    #return render_template('spinner.html', title='Loading')
-
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
